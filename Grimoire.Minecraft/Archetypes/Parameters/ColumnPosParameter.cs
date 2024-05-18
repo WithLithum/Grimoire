6292802b1decb4be8f1ce1408cfe2f7e3e7dbf1e
WithLithum <WithLithum@outlook.com>
2024-05-18 20:08:55 +0800

    feat(minecraft): ColumnPos

---
+++ Grimoire.Minecraft/Archetypes/Parameters/ColumnPosParameter.cs
@@ -0,0 +1,24 @@
+﻿namespace Grimoire.Minecraft.Archetypes.Parameters;
+
+using Grimoire;
+using Grimoire.Archetypes.Parameters;
+using Grimoire.Exceptions;
+using Grimoire.Minecraft.Models;
+
+public sealed class ColumnPosParameter : CommandParameter<ColumnPosition>
+{
+    public override ColumnPosition ReadArgument(CommandReader reader)
+    {
+        var x = BlockPosParameter.ReadPositionComponent(reader);
+        var z = BlockPosParameter.ReadPositionComponent(reader);
+
+        var result = new ColumnPosition(x, z);
+        if (!result.IsValid())
+        {
+            throw CommandFormatException.Create(MinecraftCommandErrors.MixLocalAndWorld,
+                reader);
+        }
+
+        return result;
+    }
+}
---
+++ Grimoire.Minecraft/Models/ColumnPosition.cs
@@ -0,0 +1,42 @@
+﻿namespace Grimoire.Minecraft.Models;
+
+using MineJason.Data.Coordinates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Defines a two-dimensional vector (X, Z) that represents an entire column of blocks in world space.
+/// </summary>
+public readonly record struct ColumnPosition : IValidatable
+{
+    public ColumnPosition(BlockPositionComponent x, BlockPositionComponent z)
+    {
+        X = x;
+        Z = z;
+    }
+
+    public ColumnPosition(int x, int z, BlockPositionComponentType type = BlockPositionComponentType.Absolute)
+    {
+        X = new BlockPositionComponent(x, type);
+        Z = new BlockPositionComponent(z, type);
+    }
+
+    /// <summary>
+    /// Gets the X component of this instance.
+    /// </summary>
+    public BlockPositionComponent X { get; }
+
+    /// <summary>
+    /// Gets the Y component of this instance.
+    /// </summary>
+    public BlockPositionComponent Z { get; }
+
+    public bool IsValid()
+    {
+        return X.Type == BlockPositionComponentType.Local
+            != (Z.Type == BlockPositionComponentType.Local);
+    }
+}
