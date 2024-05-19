67ecab88a5e8bf785234a68edf509c62ca54252b
WithLithum <WithLithum@outlook.com>
2024-05-19 16:11:17 +0800

    feat(archetype): Pattern archetypes

---
+++ Grimoire/Archetypes/LiteralArchetype.cs
@@ -0,0 +1,31 @@
+﻿using Grimoire.Exceptions;
+
+namespace Grimoire.Archetypes;
+
+public class LiteralArchetype : CommandArchetype
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LiteralArchetype"/> class.
+    /// </summary>
+    /// <param name="literal">The literal text to verify.</param>
+    public LiteralArchetype(string literal)
+    {
+        Literal = literal;
+    }
+    
+    /// <summary>
+    /// Gets the literal to validate.
+    /// </summary>
+    public string Literal { get; }
+    
+    public override void Read(CommandReader reader)
+    {
+        var word = reader.ReadUnquotedString();
+
+        if (!word.Equals(Literal, StringComparison.Ordinal))
+        {
+            throw CommandFormatException.Create(CommandFormatError.ExpectedWord(Literal),
+                reader);
+        }
+    }
+}
---
+++ Grimoire/Archetypes/Patterns/PatternArchetype.cs
@@ -0,0 +1,22 @@
+﻿namespace Grimoire.Archetypes.Patterns;
+
+public class PatternArchetype : CommandArchetype
+{
+    public PatternArchetype(CommandArchetypeCollection? archetypes = null)
+    {
+        if (archetypes != null)
+        {
+            Archetypes = archetypes;
+        }
+    }
+    
+    public CommandArchetypeCollection Archetypes { get; } = new();
+    
+    public override void Read(CommandReader reader)
+    {
+        foreach (var archetype in Archetypes)
+        {
+            archetype.Read(reader);
+        }
+    }
+}
